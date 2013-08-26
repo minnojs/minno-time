13253019d07fdc585b60c8d76eaeaf2f23fa3cc5
ben goldenberg <goldenbergben@gmail.com>
2013-08-26 13:16:12 +0300

    bug fix

--- examples/dscore/msgCat.js
+++ examples/dscore/msgCat.js
@@ -17,20 +17,26 @@ define(['jquery','underscore'],function($,_){
 
 		getMsg: function(score){
 
-			var array = this.mesCatArray;
+		var array = this.mesCatArray;
 			var scoreNum = parseFloat(score);
 			console.log('entering getMsg'+scoreNum);
 			console.log(array);
 			var cut = null;
 			var msg = null;
+			var rightMsg = 'error: msg was not set';
+			var set = false;
 			_.each(array, function(val,key) {
 				cut = parseFloat(val.cut);
 				msg = val.message;
 				console.log(cut);
 				console.log(msg);
-				if (scoreNum<=cut) return msg;
+				if (scoreNum<=cut && set ==false){
+					rightMsg = msg;
+					set = true;
+
+				} 
 			});
-			return msg;
+			return rightMsg;
 		}
 
 
