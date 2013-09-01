09bd595681d198ce23a6e0174cecf346c25bf5f3
eladzlot <ez.implicit@gmail.com>
2013-09-01 14:33:33 +0300

    created build process

--- build/app.build.js
+++ build/app.build.js
@@ -1,17 +1,20 @@
 //Install node.js, navigate to the js folder, and then run this command: "node r.js -o app.build.js"
+// examples @ https://github.com/jrburke/r.js/blob/master/build/example.build.js
 ({
 
   // Creates a js-optimized folder at the same folder level as your "js" folder and places the optimized project there
-  dir: "../js-optimized",
+  dir: "../dist",
+
+  appDir: '../src',
+  baseUrl: 'js',
 
   // Tells Require.js to look at main.js for all shim and path configurations
-  mainConfigFile: 'main.js',
+  mainConfigFile: '../src/js/main.js',
 
   // Modules to be optimized:
   modules: [
     {
-        name: "main"
+        name: "app/API"
     }
   ]
-
-});
+})
