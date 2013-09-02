b324ddc9de9140fd63290c6e772c74d856eb1721
ben goldenberg <goldenbergben@gmail.com>
2013-09-02 15:03:23 +0300

    comment score.log added one line of new code

--- examples/dscore/msgCat.js
+++ examples/dscore/msgCat.js
@@ -19,8 +19,8 @@ define(['jquery','underscore'],function($,_){
 
 			var array = this.mesCatArray;
 			var scoreNum = parseFloat(score);
-			console.log('entering getMsg'+scoreNum);
-			console.log(array);
+		//	console.log('entering getMsg'+scoreNum);
+		//	console.log(array);
 			var cut = null;
 			var msg = null;
 			var rightMsg = 'error: msg was not set';
@@ -30,8 +30,8 @@ define(['jquery','underscore'],function($,_){
 			_.each(array, function(val,key) {
 				cut = parseFloat(val.cut);
 				msg = val.message;
-				console.log(cut);
-				console.log(msg);
+			//	console.log(cut);
+			//	console.log(msg);
 				if (scoreNum<=cut && set ==false){
 					rightMsg = msg;
 					set = true;
