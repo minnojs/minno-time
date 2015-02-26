69a4dca639a7acc55883d22470e4e6416bc6cedf
eladzlot <eladzlot@gmail.com>
2015-02-26 09:36:52 +0200

    fix(lodash): updated to compatability version

--- src/js/config.js
+++ src/js/config.js
@@ -6,22 +6,22 @@ require.config({
   enforceDefine: true,
   baseUrl:'js',
 
-  // 3rd party script alias names (Easier to type "jquery" than "libs/jquery-1.7.2.min")
+  // 3rd party script alias names (Easier to type 'jquery' than 'libs/jquery-1.7.2.min')
   paths: {
 		//plugins
-		text: ['//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.min', "../../bower_components/requirejs-text/text"],
+		text: ['//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.min', '../../bower_components/requirejs-text/text'],
 
 		// Core Libraries
-		jquery: ["//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min","../../bower_components/jquery/jquery.min"],
-		underscore: ["//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.1.0/lodash.min","../../bower_components/lodash/dist/lodash.min"],
-		backbone: ['//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min', "../../bower_components/backbone/backbone-min"]
+		jquery: ['//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min','../../bower_components/jquery/jquery.min'],
+		underscore: ['https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.1.0/lodash.compat.min','../../bower_components/lodash/dist/lodash.compat.min'],
+		backbone: ['//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min', '../../bower_components/backbone/backbone-min']
   },
 
   // Sets the configuration for your third party scripts that are not AMD compatible
   shim: {
-      "backbone": {
-          "deps": ["underscore", "jquery"],
-          "exports": "Backbone"  //attaches "Backbone" to the window object
+      'backbone': {
+          'deps': ['underscore', 'jquery'],
+          'exports': 'Backbone'  //attaches 'Backbone' to the window object
       }
   }, // end Shim Configuration
   deps: ['jquery', 'backbone', 'underscore']
