5790bf409f9247510990ce9a7c0c9a8bfb3cca8d
eladzlot <ez.implicit@gmail.com>
2015-08-25 23:32:19 +0300

    fix(media): assert that html media is an ELEMENT_NODE

--- src/js/app/media/media_constructor.js
+++ src/js/app/media/media_constructor.js
@@ -2,7 +2,8 @@ define(function(require) {
 	var _ = require('underscore')
 		, MediaView = require('./media_view')
 		,html = require('utils/html')
-		,global = require('app/global');
+		,global = require('app/global')
+		,$ = require('jquery');
 
 	function Media(source, model){
 
@@ -22,6 +23,12 @@ define(function(require) {
 			trialData: model.trial.data,
 			stimulusData: model.get('data')
 		});
+
+		// We can get errors where the element is a text node (#3) because jquery is not set to deal with them
+		// this is a bit of a hack, and should probably move to the html.js provider
+		if ($(source.el)[0].nodeType !== Node.ELEMENT_NODE){
+			throw new Error('Media element must be an ELEMENT_NODE (#1) but was: "#' +  $(source.el)[0].nodeType + '" instead.');
+		}
 
 		// return a new media view
 		return new MediaView(source);
--- src/js/config.js
+++ src/js/config.js
@@ -16,7 +16,7 @@ define(function(){
       text: ['//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.min', "../../bower_components/requirejs-text/text"],
 
       // Core Libraries
-      jquery: ["//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min","../../bower_components/jquery/jquery.min"],
+      jquery: ["a//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min","../../bower_components/jquery/jquery"],
       underscore: ["//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.5.0/lodash.min","../../bower_components/lodash-compat/lodash.min"],
       backbone: ['//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min', "../../bower_components/backbone/backbone"]
     },
