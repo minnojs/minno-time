8973e345c2588131a6e387345d46344473057e19
eladzlot <ez.implicit@gmail.com>
2017-03-09 11:19:29 +0200

    chore: release v0.3.30

--- dist/js/utils/database/inflateProvider.js
+++ dist/js/utils/database/inflateProvider.js
@@ -108,9 +108,8 @@ define(function(require){
 						if (!_.isPlainObject(parentProp)){
 							throw new Error('Inheritance error: You tried merging an object with an non object (for "' + key + '")');
 						}
-						_.extend(childProp, parentProp);
+						child[key] = _.extend({},parentProp,childProp);
 					}
-
 				}
 			});
 
