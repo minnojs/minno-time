0cf813e8e983716079f827b5feb1380f5387c8d3
eladzlot <ez.implicit@gmail.com>
2017-12-21 09:42:25 +0200

    chore(docs): removed sequencer doc

--- 0.3/time/iat.js
+++ 0.3/time/iat.js
@@ -1,7 +1,5 @@
 // You can find the images used for this task [here](https://github.com/minnojs/minno-time/tree/gh-pages/images)
 // and the templates [here](https://github.com/minnojs/minno-time/tree/gh-pages/resources/IAT).
-//
-// ** This example is intended for educational purposes only. If you want to actually use this type of task, please see the [extensions page](https://app-prod-03.implicit.harvard.edu/implicit/common/all/js/pip/piscripts/ydocs/dist/index.html). **
 define(['pipAPI','pipScorer'], function(APIConstructor,Scorer) {
     // This wrapper is neccesary in order to activate the API
 
