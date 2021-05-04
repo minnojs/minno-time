c58e53129f7298e63eefff3f0d82e7e3d5fb0bd6
eladzlot <ez.implicit@gmail.com>
2021-05-04 22:59:11 +0300

    feat(actions): binded timeout to framerate

--- src/app/trial/input/bindings/timeout.js
+++ src/app/trial/input/bindings/timeout.js
@@ -1,7 +1,16 @@
 import randomize from './simpleRandomize';
 import stream from 'mithril-stream';
+import fastdom from 'fastdom';
+import now from '../now';
 
 export default timeout;
+
+/*
+ * We set timeout to trigger half a frame before the target time.
+ * This is done so that on overage we hit as close as possible to the true duration
+ * `offset` is updated dynamically by correctOfset() below
+ **/
+var offset = 8;
 
 function timeout(inputObj){
     var $listener = stream();
@@ -10,7 +19,11 @@ function timeout(inputObj){
 
     $listener.end.map(cancel);
 
-    if (duration) setTimeout($listener.bind(null, {}), duration);
+    // start timeout the same time that current visual stimuli occur
+    if (duration) fastdom.mutate(function(){
+        timeoutID = setTimeout($listener.bind(null, {}), duration-offset);
+    });
+
     else $listener({}); // listener is already registered with $events so this should be immidiate
 
     return $listener;
@@ -19,3 +32,26 @@ function timeout(inputObj){
         clearTimeout(timeoutID);
     }
 }
+
+// compute true frame rate for this specific machine
+// and set offset to half of that
+(function correctOfset(){
+    var itterations = 50;
+    var a,b, results = [];
+    requestAnimationFrame(function(){
+        a = now();
+        update();
+    });
+
+    function update(){
+        requestAnimationFrame(function(){
+            b = now();
+            results.push(b-a)
+            a = b;
+            if (results.length < itterations)  return setTimeout(update);
+            else offset = mean(results)/2; // we want an offset of half of the frame rate, so on average we hit the closest frame possible to the target.
+        });   
+    }
+
+    function mean(arr){return arr.reduce(function(v,s){return v+s},0)/arr.length}
+})();
